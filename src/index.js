2cf3a96c75a27982dcf80bd357b613f883b4ce56
Matthew Bridges <mattbridgesbusiness@gmail.com>
2017-10-28 16:29:09 +0100

    Add react router to project, and scaffold routes to be used in the blog

--- src/index.js
+++ src/index.js
@@ -2,14 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-import App from './components/app';
 import reducers from './reducers';
+import BlogIndex from './components/blog_index';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <div>
+        <h1>React Blog</h1>
+        <Route path='/' component={BlogIndex} />
+        {/* {<Route path='/blog/:id' component={BlogDetail} />} */}
+        {/* <Route path='/blog/new' component={BlogNew} /> */}
+      </div>
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
